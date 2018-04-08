4c35687e1203fe4f7a3a61792f3c4c31077e1f88
dan make <danmakes2022@gmail.com>
2018-04-08 15:30:57 +0200

    add dynamic board rendering

--- src/app/components/Connect4Board/Connect4Board.jsx
+++ src/app/components/Connect4Board/Connect4Board.jsx
@@ -1,86 +1,53 @@
 import React from 'react';
 import './Connect4Board.scss'
 
+import Connect4Cell from '../Connect4Cell/Connect4Cell.jsx';
+
 export default class Connect4Board extends React.Component {
   constructor() {
     super();
     this.state = {
     }; 
   }
-  
+
   render() {
+    let board = this.props.board;
+
+    let rowInput = board[0].map((col, colIndex) => {
+      return (
+        <div className="Connect4Board__cell" key={colIndex} id={colIndex}>
+          <div className="col" id={colIndex}></div>
+        </div>
+      )
+    });
+
+    let rows = board.map((row, rowIndex) => {
+      return (
+        <div className="row"  key={rowIndex} >
+          {row.map((cell, colIndex) => (
+            <div className="Connect4Board__cell" key={colIndex} id={`${rowIndex}-${colIndex}`} >
+              <Connect4Cell col={colIndex} row={rowIndex} />
+            </div>
+          ))}
+        </div>
+      )
+    });
+          
     return (
       <div className="connect4__board">
         <p>Hello {this.props.config.player1} and {this.props.config.player2}!</p>
 
         <div className="gameboard">
           <div className="row input">
-            <div className="col" id="0"></div>
-            <div className="col" id="1"></div>
-            <div className="col" id="2"></div>
-            <div className="col" id="3"></div>
-            <div className="col" id="4"></div>
-            <div className="col" id="5"></div>
-            <div className="col" id="6"></div>
+            {rowInput}
           </div>
           <div className="playField">
-            <div className="row">
-              <div className="col" id="row5col0"></div>
-              <div className="col" id="row5col1"></div>
-              <div className="col" id="row5col2"></div>
-              <div className="col" id="row5col3"></div>
-              <div className="col" id="row5col4"></div>
-              <div className="col" id="row5col5"></div>
-              <div className="col" id="row5col6"></div>
-            </div>
-            <div className="row">
-              <div className="col" id="row4col0"></div>
-              <div className="col" id="row4col1"></div>
-              <div className="col" id="row4col2"></div>
-              <div className="col" id="row4col3"></div>
-              <div className="col" id="row4col4"></div>
-              <div className="col" id="row4col5"></div>
-              <div className="col" id="row4col6"></div>
-            </div>
-            <div className="row">
-              <div className="col" id="row3col0"></div>
-              <div className="col" id="row3col1"></div>
-              <div className="col" id="row3col2"></div>
-              <div className="col" id="row3col3"></div>
-              <div className="col" id="row3col4"></div>
-              <div className="col" id="row3col5"></div>
-              <div className="col" id="row3col6"></div>
-            </div>
-            <div className="row">
-              <div className="col" id="row2col0"></div>
-              <div className="col" id="row2col1"></div>
-              <div className="col" id="row2col2"></div>
-              <div className="col" id="row2col3"></div>
-              <div className="col" id="row2col4"></div>
-              <div className="col" id="row2col5"></div>
-              <div className="col" id="row2col6"></div>
-            </div>
-            <div className="row">
-              <div className="col" id="row1col0"></div>
-              <div className="col" id="row1col1"></div>
-              <div className="col" id="row1col2"></div>
-              <div className="col" id="row1col3"></div>
-              <div className="col" id="row1col4"></div>
-              <div className="col" id="row1col5"></div>
-              <div className="col" id="row1col6"></div>
-            </div>
-            <div className="row">
-              <div className="col" id="row0col0"></div>
-              <div className="col" id="row0col1"></div>
-              <div className="col" id="row0col2"></div>
-              <div className="col" id="row0col3"></div>
-              <div className="col" id="row0col4"></div>
-              <div className="col" id="row0col5"></div>
-              <div className="col" id="row0col6"></div>
-            </div>
+            {rows}
           </div>
-        </div>
 
-      </div>);
+        </div>
+        <br/>
+      </div>
+    );
   }
 }
--- src/app/components/Connect4Game/Connect4Game.jsx
+++ src/app/components/Connect4Game/Connect4Game.jsx
@@ -4,12 +4,29 @@ import Connect4Board from '../Connect4Board/Connect4Board.jsx';
 import './Connect4Game.scss'
 
 export default class Connect4Game extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      showInstructions: false
-    }; 
+  /* Board */
+  initBoard(cols, rows) {
+    const board = [];
+    for (let i = 0; i < cols; i++) {
+      board[i] = [];
+      for (let j = 0; j < rows; j++) {
+        board[i][j] = null;
+      }
+    }
+    return board;
   }
+
+  componentWillMount() {
+    const board = this.initBoard(this.props.config.rows, this.props.config.cols);
+    this.setState({
+      showInstructions: false,
+      board,
+      currentPlayer: "PLAYER_1",
+      state: "_RUNNING"
+    });
+  }
+
+
 
   toggleInstructions() {
     this.setState({
@@ -21,7 +38,8 @@ export default class Connect4Game extends React.Component {
     return (
       <div className="connect4__game">
 
-        <Connect4Board config={this.props.config}/>
+        <Connect4Board state={this.state.state} board={this.state.board} 
+            config={this.props.config}/>
 
         <div className="btn__container">
           <button className="btn__primary btn__small" onClick={this.props.onGameStop}>
